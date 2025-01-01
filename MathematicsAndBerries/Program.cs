acef0d0b6503bcd1d336cd09aef5fa3ce7942eff
Shamsiddin <102223307+ext1z@users.noreply.github.com>
2025-01-02 00:27:17 +0500

    Update Program.cs

--- MathematicsAndBerries/Program.cs
+++ MathematicsAndBerries/Program.cs
@@ -11,20 +11,7 @@ class Program
 {
     static void Main(string[] args)
     {
-        string? input = Console.ReadLine()!;
 
-        string[] splited = input.Split(' ', '\n');
-
-
-        int[] arrayOfint = new int[splited.Length];
-
-
-        for (int i = 0; i < splited.Length; i++)
-        {
-            arrayOfint[i] = int.Parse(splited[i]);
-        }
-
-        Console.WriteLine($"{arrayOfint[0]} {arrayOfint[3]}");
         
     }
 }
